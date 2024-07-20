33cd64b8f952bf8747991eab0dba91dbcb4e0b60
Focaan <145490871+Focaan@users.noreply.github.com>
2024-07-20 22:47:01 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -10,6 +10,7 @@ fcn = {}
 fcn.kickMessage = "[fcn_afksystem] \n\n ❌You Have Been kicked for AFK! \n After FiveM restart you will be able to connect again."  -- The kick "reason"/message
 fcn.secondsUntilKick = 1800 -- After what time the player will get kicked
 fcn.kickWarning = true -- If it will show the player a notify when he will be kicked
+fcn.enableLogging = true -- Disable/Enable discord logging in sv_config/server.lua
 -- AFK RESET SETTINGS -- 
 fcn.afkResetEnabled = true -- Disable/Enable the afkreset command
 fcn.afkResetCommand = "afkreset" -- Name of the command Default: /afkreset
